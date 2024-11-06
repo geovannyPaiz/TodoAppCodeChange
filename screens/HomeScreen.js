cb3ac1e01676b6853255dba53cfbdafc1cf69653
Samuel Paiz <geovanny_paiz@hotmail.com>
2024-11-06 13:16:57 -0600

    add: list

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,26 +2,11 @@ import React from "react";
 import { StyleSheet, View, FlatList, Button, Text } from "react-native";
 
 const taskList = [
-  {
-    id: 1,
-    nombre: "task 1",
-  },
-  {
-    id: 2,
-    nombre: "task 2",
-  },
-  {
-    id: 3,
-    nombre: "task 3",
-  },
-  {
-    id: 4,
-    nombre: "task 4",
-  },
-  {
-    id: 5,
-    nombre: "task 5",
-  },
+  { id: 1, nombre: "task 1" },
+  { id: 2, nombre: "task 2" },
+  { id: 3, nombre: "task 3" },
+  { id: 4, nombre: "task 4" },
+  { id: 5, nombre: "task 5" },
 ];
 
 export default function HomeScreen({ navigation }) {
@@ -29,7 +14,7 @@ export default function HomeScreen({ navigation }) {
     taskList = taskList.filter((element) => element.id !== id);
   };
 
-  const Item = (title, id) => {
+  const Item = ({ title, id }) => {
     return (
       <View>
         <Text>{title}</Text>
@@ -40,15 +25,11 @@ export default function HomeScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {/* Here add your Code for Task List and button to navigate to add new tasks, also remember todo's should be able to be deleted. */}
-      {/* <FlatList
-        data={[]}
+      <FlatList
+        data={taskList}
         renderItem={({ item }) => <Item title={item.nombre} id={item.id} />}
-        keyExtractor={(item) => item.id}
-      /> */}
-      {taskList.map((element) => (
-        <Item title={element.nombre} id={element.id} />
-      ))}
+        keyExtractor={(item) => item.id.toString()}
+      />
       <Button title="Add task" onPress={() => navigation.navigate("AddTask")} />
     </View>
   );
